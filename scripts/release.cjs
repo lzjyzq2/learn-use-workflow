57695c667136845ddb3bc524ca3d8e09e9c9f3df
lzjyzq2 <lzjyzq2@hotmail.com>
2022-11-03 19:23:50 +0800

    feat: new ci

--- scripts/release.cjs
+++
@@ -1,5 +0,0 @@
-const semanticRelease = require('semantic-release');
-const {WritableStreamBuffer} = require('stream-buffers');
-
-const stdoutBuffer = WritableStreamBuffer();
-const stderrBuffer = WritableStreamBuffer();
