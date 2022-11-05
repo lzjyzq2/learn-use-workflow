107da3c6abf97808187b97c532da31f835771012
lzjyzq2 <lzjyzq2@hotmail.com>
2022-11-06 00:49:23 +0800

    fix: fix shell

--- bash/release.ps1
+++ bash/release.ps1
@@ -13,12 +13,11 @@
 
 # $updater | ConvertTo-Json | Write-Output
 
-param ($version = $(throw "version is required."))
-# , $notes = $(throw "notes is required.")
+param ($version = $(throw "version is required."), $notes = $(throw "notes is required."))
 
 # set env
-$env:version=$version
-# $env:notes=$notes
+$env:version = $version
+$env:notes=$notes
 
 # Build Tauri
 npx tauri build
