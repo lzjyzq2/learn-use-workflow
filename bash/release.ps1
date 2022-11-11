7000b88780b5ffa36e7c968c11663530736c9a1d
lzjyzq2 <1462678018@qq.com>
2022-11-11 17:32:51 +0800

    fix: fix config

--- bash/release.ps1
+++ bash/release.ps1
@@ -1,2 +1,12 @@
-Write-Output 'Test'
-exit 1
+param ($version = $(throw "version is required."), $notes = $(throw "notes is required."))
+
+# set env
+$env:version = $version
+$env:notes=$notes
+
+# Build Tauri
+npx tauri build
+
+# generate updater.json
+npm run updater $version $notes
+exit 0
